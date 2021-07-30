c5cf4dc7a002fa91e34ae1a66e5ae7c39034cd77
Farooq Umer <farooq.tfora@gmail.com>
2021-07-30 21:09:10 +0530

    initial commit

--- setup.py
+++ setup.py
@@ -1,34 +1,26 @@
 from distutils.core import setup
 setup(
-    # How you named your package folder (MyLib)
     name='tfora_social_auth',
-    packages=['tfora_social_auth'],   # Chose the same as "name"
-    version='0.1',      # Start with a small number and increase it with every change you make
-    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    packages=['tfora_social_auth'], 
+    version='0.1',     
     license='MIT',
-    # Give a short description about your library
-    description='TYPE YOUR DESCRIPTION HERE',
-    author='Ummer Farooq',                   # Type in your name
-    author_email='farooq.tfora@gmail.com',      # Type in your E-Mail
-    # Provide either the link to your github or to your website
-    url='https://github.com/user/reponame',
-    # I explain this later on
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
-    # Keywords that define your package best
-    keywords=['django', 'social login', 'allauth', 'rest_auth'],
-    install_requires=[            # I get to this in a second
+    description='Easy django rest auth integration for social applications (Currently supports google and facebook)',
+    author='Ummer Farooq',                   
+    author_email='farooq.tfora@gmail.com',      
+    url='https://github.com/pvfarooq/tfora_social_auth',
+    download_url='https://github.com/pvfarooq/tfora_social_auth/archive/refs/tags/0.1.tar.gz',
+    keywords=['django', 'social login', 'allauth', 'rest_auth','google login','facebook login'],
+    install_requires=[            
         'facebook-sdk',
         'google-auth',
-        'requests'
+        'requests',
+        'djangorestframework'
     ],
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 3 - Alpha',
-        # Define that your audience are developers
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        # Specify which pyhton versions that you want to support
+        'License :: OSI Approved :: MIT License', 
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
